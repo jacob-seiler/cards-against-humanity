4909dc260ce005ff063d12caa6acedb0c8cef09c
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-23 13:25:00 -0500

    Changed to actual function from arrow function

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -2,39 +2,7 @@ import type { NextApiRequest } from "next";
 import type { NextApiResponseServerIO } from "../../types/next";
 import { Server } from 'socket.io'
 
-// const SocketHandler = (_req: NextApiRequest, res: NextApiResponseServerIO) => {
-//   if (res.socket.server.io) {
-//     console.log('socket.io already running')
-//     res.end()
-//     return
-//   }
-
-//   console.log('*First use, starting socket.io')
-
-//   const io = new Server(res.socket.server as any)
-
-//   io.on('connection', socket => {
-//     socket.broadcast.emit('a user connected')
-//     socket.on('hello', msg => {
-//       socket.emit('hello', 'world!')
-//     })
-
-//     socket.on('message', message => {
-//       console.log('m received', message)
-//     })
-//   })
-
-//   // io.on('message', message => {
-//   //   console.log('m received', message)
-//   // })
-
-//   res.socket.server.io = io
-//   res.end()
-// }
-
-// export default SocketHandler
-
-const SocketHandler = (_req: NextApiRequest, res: NextApiResponseServerIO) => {
+export default function SockerHandler(_req: NextApiRequest, res: NextApiResponseServerIO) {
   if (res.socket.server.io) {
     console.log('Socket is already running')
   } else {
@@ -51,5 +19,3 @@ const SocketHandler = (_req: NextApiRequest, res: NextApiResponseServerIO) => {
   }
   res.end()
 }
-
-export default SocketHandler
